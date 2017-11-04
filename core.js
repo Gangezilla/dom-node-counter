7d6f2e5e9da999dc178bdb618b1851b47635440e
Scott <scottgangemi@gmail.com>
2017-11-04 20:43:20 +1100

    work on making this work...

--- core.js
+++ core.js
@@ -1,5 +1,4 @@
-// Unique ID for the className.
-var MOUSE_VISITED_CLASSNAME = 'dom-node__mouse-visited';
+var mouseVisitedClassname = 'dom-node__mouse-visited';
 
 // Previous dom, that we want to track, so we can remove the previous styling.
 var prevDOM = null;
@@ -14,9 +13,29 @@ document.addEventListener('mouseover', function (event) {
   // popup.style.left = mouseX;
   if (srcElement) {
     if (prevDOM !== null) {
-      prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
+      prevDOM.classList.remove(mouseVisitedClassname);
     }
-    srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
+    srcElement.classList.add(mouseVisitedClassname);
     prevDOM = srcElement;
   }
 }, false);
+
+function handleMessage(request, sender, sendResponse) {
+  console.log("Message from the content script: " +
+    request);
+  sendResponse({response: "Response from background script"});
+}
+
+// chrome.runtime.onMessage.addListener(handleMessage);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    console.log(request);
+  });
+
+
+// trigger only when the button gets clicked.
+// work only on the tab where the button gets clicked.
+// show a little box next to the mouse (top right) with DOM info. 
