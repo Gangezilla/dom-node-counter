14db65395a9a274a339f3752367eca1b16183b4e
Scott <scottgangemi@gmail.com>
2017-11-06 20:57:48 +1100

    add icons, finish up v1

--- background.js
+++ background.js
@@ -1,20 +1,14 @@
-var isEnabled = false;
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var browser = browser || chrome;
+let isEnabled = false;
+chrome.browserAction.onClicked.addListener((tab) => {
   if (isEnabled) {
     isEnabled = false;
   } else {
     isEnabled = true;
   }
   
-  var request = { isEnabled };
+  const request = { isEnabled };
   
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, request, (response) => {
-      console.log('callback');
-    });
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    browser.tabs.sendMessage(tabs[0].id, request, (response) => {});
   });
 });
-
-// var elem = document.getElementById("myDiv");
-// elem.parentNode.removeChild(elem);
--- core.js
+++ core.js
@@ -6,15 +6,19 @@ let highlightedNode = null;
 const domNodeCounter = (event) => {
   highlightedNode = event.srcElement;
   document.body.style.position = 'relative';
+
   const popup = document.getElementById('dom-node__popup');
   let mouseY = event.pageY;
   popup.style.top = `${mouseY+10}px`;
   let mouseX = event.pageX;
   popup.style.left = `${mouseX+10}px`;
+
   let domNodes = highlightedNode.getElementsByTagName('*');
   let totalDomNodes = domNodes.length;
   console.log(domNodes);
+
   popup.innerHTML=`<span>DOM Nodes: ${totalDomNodes}</span></br><span>Check your console to see all DOM Nodes.</span>`
+
   if (highlightedNode) {
     if (prevDOM !== null) {
       prevDOM.classList.remove(mouseVisitedClassname);
@@ -23,6 +27,7 @@ const domNodeCounter = (event) => {
     prevDOM = highlightedNode;
   }
 }
+
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => {
     const popupElement = document.createElement('div');
@@ -32,16 +37,16 @@ chrome.runtime.onMessage.addListener(
       document.body.appendChild(popupElement);
       document.addEventListener('mouseover', domNodeCounter);
     } else {
-      // remove popupElement in here. list.removeChild(list.childNodes[0]); 
-      // popupElement.removeChild(popupElement.childNodes[0])
-      document.getElementById('dom-node__popup').remove();
-      highlightedNode.classList.remove(mouseVisitedClassname);
+      if (document.getElementById('dom-node__popup')) {
+        document.getElementById('dom-node__popup').remove();
+      }
+      if (highlightedNode) {
+        highlightedNode.classList.remove(mouseVisitedClassname);
+      }
       document.removeEventListener('mouseover', domNodeCounter);
     }
   });
 
   // get better at drilling down into nested structure somehow...
-  // remove element once it's been created.
   // styling
-  // icon
   // on/off over iconwhen its on/off.
